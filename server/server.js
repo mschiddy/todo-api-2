4fac4fd5eefcb22cec89dd36bda8fe4a07690cec
mschiddy <mschiddy@gmail.com>
2017-08-28 11:09:52 -0600

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,7 @@
+/*Restart on Udemy video 74, testing POST routes and 
+ * adding testing express middleware/ dependencies;
+ * currently, GET doesn't work with :id */
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
@@ -24,31 +28,29 @@ app.post('/todos', (req, res) => {
 	});
 });
 
-/*
 app.get('/todos', (req, res) => {
-	todo.find().then((todos) => {
+	Todo.find().then((todos) => {
 	res.send({todos});
 }, (e) => {
 	res.status(400).send(e);
 	});
 });
-*/
-/*
+
 app.get('/todos/:id', (req, res) => {
 	var id = req.params.id;
 	if(!ObjectID.isValid(id)) {
 		return res.status(404).send();
 	}
-	todo.findById(id).then((todo) => {
+	Todo.findById(id).then((todo) => {
 		if(!todo) {
 			return res.status(404).send();
-		}   res.send({Todo});
+		}   res.send({todo});
 	}).catch((e) => {
 		res.status(400).send();
 	});
-});*/
+});
 
 app.listen(port, () => {
 	console.log(`Started on port ${port}`);
 })
-//module.exports = {app};
+module.exports = {app};
