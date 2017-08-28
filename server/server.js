cc902dda4e3a6beee070e99eaedba246dad70725
mschiddy <mschiddy@gmail.com>
2017-08-28 12:53:05 -0600

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -10,8 +10,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-//const port = process.env.PORT || 3000;
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
